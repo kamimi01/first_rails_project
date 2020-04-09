6b3077fb19ecf469072213a1d486fbec4d89de09
kamimi01 <awakaru0122@gmail.com>
2020-04-09 14:59:39 +0900

    message model

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_message-board_session'
+Rails.application.config.session_store :cookie_store, key: '_message_board_session'
